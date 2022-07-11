d13c200ed8875a9d90fc6bed6d0f477907b2f4e4
Don Reeves <donjreeves@gmail.com>
2022-07-11 18:32:23 -0400

    added app content to client side

---
+++ server/routes/posts.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+
+const router = express.Router();
+import auth from "../middleware/auth.js";
+
+router.get('/creator', getPostsByCreator);
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPost);
+
+router.post('/', createPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+router.post('/:id/commentPost', commentPost);
+
+export default router;
